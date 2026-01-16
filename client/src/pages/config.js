3180aba5f8e9d5b41c0b2fd6acdfa2e7aa6478c2
Gaurav-kangane <163861574+Gaurav-kangane@users.noreply.github.com>
2026-01-16 22:46:49 +0530

    Update config.js

--- client/src/pages/config.js
+++ client/src/pages/config.js
@@ -1,4 +1,4 @@
-const API_BASE_URL = "afccf7968dba44f32b43b8fc58c67c95-733257197.us-east-1.elb.amazonaws.com"
+const API_BASE_URL = "http://afccf7968dba44f32b43b8fc58c67c95-733257197.us-east-1.elb.amazonaws.com"
 //const API_BASE_URL = "http://backend.515800584282.realhandsonlabs.net";
 // export default API_BASE_URL;
 // const API_BASE_URL = process.env.REACT_APP_API_BASE_URL || "http://backend";
